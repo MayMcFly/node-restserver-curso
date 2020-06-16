deef436abec3de46450780904123c443543ac418
maygallegos <mg.onze@gmail.com>
2020-06-16 11:28:44 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://cafe-user:K1g7zPFyqMdbLUOL@clustermay-lk2me.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 //*/
 
